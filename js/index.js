9d5d5fea79f7a771cddef1b4d1d12dd0351fd10b
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-19 15:12:31 +0530

    Adding project

--- js/index.js
+++ js/index.js
@@ -1,22 +1,39 @@
-$(document).ready(function () {
-  $(".sidenav").sidenav();
-  $(".slider").slider({
-    fullWidth: true,
-  });
+$(document).ready(function() {
+    $(".sidenav").sidenav();
+    $(".slider").slider({
+        fullWidth: true,
+    });
 });
 
 //Get the button:
 mybutton = document.getElementById("gotopbtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
+window.onscroll = function() {
+    scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
+
+$('.carousel[data-type="multi"] .item').each(function() {
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < 2; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
